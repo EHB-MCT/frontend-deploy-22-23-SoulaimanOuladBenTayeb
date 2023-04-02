8be8b6e232920579da4ec3a52d18bdf1fa941545
SoulaimanOuladBenTayeb <56120310+SoulaimanOuladBenTayeb@users.noreply.github.com>
2023-04-02 16:20:50 +0200

    add group animations

--- three-app/src/components/Shape.jsx
+++ three-app/src/components/Shape.jsx
@@ -7,6 +7,7 @@ function Shape() {
   const spherePurpleRef = useRef();
   const torusRef = useRef();
   const boxRef = useRef();
+  const groupRef = useRef();
 
   useFrame((state, delta) => {
     if (sphereRef.current) {
@@ -21,36 +22,41 @@ function Shape() {
     if (boxRef.current) {
         boxRef.current.rotation.y += delta;
     }
+    if (groupRef.current) {
+        groupRef.current.rotation.y += 0.003;
+    }
   });
 
   return (
     <>
       <OrbitControls rotateSpeed={2} />
       
-      <mesh ref={sphereRef} position={[-2, 0, 0]} rotation-x={-Math.PI * 0.5 } scale={1}>
-        <sphereGeometry />
-        <meshBasicMaterial color="blue" wireframe />
-      </mesh>
-      
-      <mesh ref={boxRef} position={[2, 0, 0]} rotation-y={-Math.PI * 0.3} scale={1.5}>
-        <boxGeometry />
-        <meshBasicMaterial color="red" wireframe/>
-      </mesh>
+      <group ref={groupRef}>
+        <mesh ref={sphereRef} position={[-2, 0, 0]} rotation-x={-Math.PI * 0.5 } scale={1}>
+            <sphereGeometry />
+            <meshBasicMaterial color="blue" wireframe />
+        </mesh>
+        
+        <mesh ref={boxRef} position={[2, 0, 0]} rotation-y={-Math.PI * 0.3} scale={1.5}>
+            <boxGeometry />
+            <meshBasicMaterial color="red" wireframe/>
+        </mesh>
 
-      <mesh ref={spherePurpleRef} position={[0, 0, 0]} rotation-z={0} scale={0.5}>
-        <sphereGeometry />
-        <meshBasicMaterial color="purple" wireframe/>
-      </mesh>
+        <mesh ref={spherePurpleRef} position={[0, 0, 0]} rotation-z={0} scale={0.5}>
+            <sphereGeometry />
+            <meshBasicMaterial color="purple" wireframe/>
+        </mesh>
 
-      <mesh ref={torusRef} position={[0, 0, 0]} rotation-y={-Math.PI * 0.7} scale={0.5}>
-        <torusGeometry />
-        <meshBasicMaterial color="yellow"/>
-      </mesh>
+        <mesh ref={torusRef} position={[0, 0, 0]} rotation-y={-Math.PI * 0.7} scale={0.5}>
+            <torusGeometry />
+            <meshBasicMaterial color="yellow"/>
+        </mesh>
 
-      <mesh position={[0, -1, 0]} rotation-x={-Math.PI * 0.5} scale={7}>
-        <planeGeometry />
-        <meshBasicMaterial color="green" />
-      </mesh>
+        <mesh position={[0, -1, 0]} rotation-x={-Math.PI * 0.5} scale={7}>
+            <planeGeometry />
+            <meshBasicMaterial color="green" />
+        </mesh>
+      </group>
     </>
   )
 }
