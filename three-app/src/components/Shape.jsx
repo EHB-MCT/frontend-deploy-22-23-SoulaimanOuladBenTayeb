66d18bb707cdb3c65cc8d7abffcec537ac1d91f2
SoulaimanOuladBenTayeb <56120310+SoulaimanOuladBenTayeb@users.noreply.github.com>
2023-04-02 16:15:31 +0200

    animation frontend R3F

--- three-app/src/components/Shape.jsx
+++ three-app/src/components/Shape.jsx
@@ -1,54 +1,58 @@
-import {OrbitControls} from '@react-three/drei';
-import { useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
+import { useRef } from 'react';
 
 function Shape() {
-    return(
-        <>
-            <OrbitControls rotateSpeed={2}></OrbitControls>
+  const sphereRef = useRef();
+  const spherePurpleRef = useRef();
+  const torusRef = useRef();
+  const boxRef = useRef();
 
-            <mesh position={ [ -2, 0, 0 ] } rotation-x={0} scale={1}>
-                <sphereGeometry/>
-                <meshBasicMaterial color="blue"/>
-                
-            </mesh>
-            
-            <mesh position={ [ 2, 0, 0 ] } rotation-x={0} scale={1.5}>
-                <boxGeometry />
-                <meshBasicMaterial color="red" />
-            </mesh>
+  useFrame((state, delta) => {
+    if (sphereRef.current) {
+      sphereRef.current.rotation.y += 0.01;
+    }
+    if (spherePurpleRef.current) {
+        spherePurpleRef.current.rotation.z += -0.005;
+    }
+    if (torusRef.current) {
+        torusRef.current.rotation.x += -0.005;
+    }
+    if (boxRef.current) {
+        boxRef.current.rotation.y += delta;
+    }
+  });
 
-            <mesh position={ [ 0, 0, 0 ] } rotation-x={0} scale={0.5}>
-                <sphereGeometry/>
-                <meshBasicMaterial color="purple"/>
-            
-                
-            </mesh>
+  return (
+    <>
+      <OrbitControls rotateSpeed={2} />
+      
+      <mesh ref={sphereRef} position={[-2, 0, 0]} rotation-x={-Math.PI * 0.5 } scale={1}>
+        <sphereGeometry />
+        <meshBasicMaterial color="blue" wireframe />
+      </mesh>
+      
+      <mesh ref={boxRef} position={[2, 0, 0]} rotation-y={-Math.PI * 0.3} scale={1.5}>
+        <boxGeometry />
+        <meshBasicMaterial color="red" wireframe/>
+      </mesh>
 
-            <mesh position={ [ 0, 0, 0 ] } rotation-x={0} scale={0.5}>
-                <torusGeometry/>
-                <meshBasicMaterial color="yellow"/>
-            
-                
-            </mesh>
-            
-            <mesh position={ [ 0, -1, 0 ] } rotation-x={-Math.PI * 0.5} scale={7}>
-                <planeGeometry />
-                <meshBasicMaterial color="green" />
-            </mesh>
+      <mesh ref={spherePurpleRef} position={[0, 0, 0]} rotation-z={0} scale={0.5}>
+        <sphereGeometry />
+        <meshBasicMaterial color="purple" wireframe/>
+      </mesh>
 
-        </>
-        
-        
-    )
+      <mesh ref={torusRef} position={[0, 0, 0]} rotation-y={-Math.PI * 0.7} scale={0.5}>
+        <torusGeometry />
+        <meshBasicMaterial color="yellow"/>
+      </mesh>
 
-    useFrame(() =>
-    {
-        console.log('tick')
-    })
-    
-    
+      <mesh position={[0, -1, 0]} rotation-x={-Math.PI * 0.5} scale={7}>
+        <planeGeometry />
+        <meshBasicMaterial color="green" />
+      </mesh>
+    </>
+  )
 }
 
-
-
-export default Shape
+export default Shape;
